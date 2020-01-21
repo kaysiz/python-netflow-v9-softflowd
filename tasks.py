559cfc39ec6162078e75ebae250b517165e73688
Kudakwashe Siziva <ksiziva@gmail.com>
2020-01-21 09:27:53 +0200

    Setup RabbitMQ and Celery

--- tasks.py
+++ tasks.py
@@ -1,9 +1,13 @@
 from celery import Celery
 
 # celery instance
-app = Celery('tasks', backend='amqp', broker='amqp://')
+app = Celery('tasks', backend='amqp', broker='amqp://admin')
 
 
 @app.task
 def add(x, y):
     return x + y
+
+
+@app.task
+def analyse_json(file):
